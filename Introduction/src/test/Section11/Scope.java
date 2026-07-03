c67b576205719b502d1a6bf5b5e95edf6f741cc9
lauraenria <enria.laura2@gmail.com>
2026-07-03 13:13:01 +0200

    feat(exercise): complete scope practice exercise
    - Count global page links and limit scope to macro footer

--- Introduction/src/test/Section11/Scope.java
+++ Introduction/src/test/Section11/Scope.java
@@ -1,5 +1,27 @@
-package test.Section11;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scope {
 
+    public static void main(String[] args) {
+
+        // 1. Give me the count of links on the page.
+        // a
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://qaclickacademy.com/practice.php");
+
+        int allLinks = driver.findElements(By.tagName("a")).size();
+
+        System.out.println("Totale links nella pagina: " + allLinks);
+
+        WebElement footerdriver = driver.findElement(By.id("gf-BIG"));
+
+        int footerLinks = footerdriver.findElements(By.tagName("a")).size();
+
+        System.out.println("Links nel footer: " + footerdriver.findElements(By.tagName("a")).size());
+    }
 }
