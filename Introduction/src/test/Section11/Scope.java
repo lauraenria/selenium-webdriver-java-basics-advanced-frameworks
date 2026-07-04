9345261d205f9ff0176bc66572d1ff2b49bd7ce2
lauraenria <enria.laura2@gmail.com>
2026-07-04 13:36:38 +0200

    feat: add lessons 97-100 WebDriver scope exercises
    - Add practice exercise to print links count in the page
    - Add WebDriver scope limitation examples
    - Add optimized solution to open links in separate tabs
    - Add child tabs title handling with optimized while loop

--- Introduction/src/test/Section11/Scope.java
+++ Introduction/src/test/Section11/Scope.java
@@ -1,4 +1,10 @@
+package test.Section11;
+
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,21 +13,65 @@ public class Scope {
 
     public static void main(String[] args) {
 
-        // 1. Give me the count of links on the page.
-        // a
-
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://qaclickacademy.com/practice.php");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
         int allLinks = driver.findElements(By.tagName("a")).size();
 
-        System.out.println("Totale links nella pagina: " + allLinks);
+        // 1. Count all links on the page
+        System.out.println("Total number of links on the page: " + allLinks);
 
+        // 2. Limit WebDriver scope to footer
         WebElement footerdriver = driver.findElement(By.id("gf-BIG"));
 
+        // Count links in the footer
         int footerLinks = footerdriver.findElements(By.tagName("a")).size();
 
-        System.out.println("Links nel footer: " + footerdriver.findElements(By.tagName("a")).size());
+        System.out.println("Links in the footer: " + footerLinks);
+
+        // 3. Limit scope to first column of footer
+        WebElement firstColumnDriver = footerdriver.findElement(By.xpath("//table[@class='gf-t']//tr//td[1]/ul"));
+
+        // Count links in the first column of the footer
+        int firstColumnDriverLinksCount = firstColumnDriver.findElements(By.tagName("a")).size();
+
+        System.out.println("Links in the first column of the footer: " + firstColumnDriverLinksCount);
+
+        // 4. Click each link in the column and check if the pages are opening
+
+        for (int i = 1; i < firstColumnDriverLinksCount; i++) {
+
+            // Open link in a new tab using Ctrl + Enter
+            String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+
+            firstColumnDriver
+                    .findElements(By.tagName("a"))
+                    .get(i)
+                    .sendKeys(clickOnLinkTab);
+
+            // try {
+            // Thread.sleep(0000);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // }
+
+        }
+
+        Set<String> allWindows = driver.getWindowHandles();
+
+        Iterator<String> iteratorWindows = allWindows.iterator();
+
+        while (iteratorWindows.hasNext()) {
+
+            String windowID = iteratorWindows.next();
+
+            driver.switchTo().window(windowID);
+
+            System.out.println(driver.getTitle());
+
+        }
+
+        driver.quit();
     }
 }
